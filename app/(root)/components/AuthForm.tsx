89c41b7ce4851eb467a23e8c717c2296b8f4bc52
jaeyeonjung <jaeyeonjung3@gmail.com>
2025-07-17 16:49:40 +0900

    ~1:40:24 (Register Functionality)

--- app/(root)/components/AuthForm.tsx
+++ app/(root)/components/AuthForm.tsx
@@ -6,6 +6,7 @@ import Input from "@/app/components/inputs/Input";
 import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from 'react-icons/bs';
+import axios from 'axios';
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -39,7 +40,7 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
-      // Axios Register
+      axios.post("/api/register", data);
     }
 
     if (variant === "LOGIN") {
